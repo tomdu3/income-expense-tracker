d713e8a5161ebce5d9f669273f8da6611669e878
Tomislav Dukez <tomdu3@ymail.com>
2025-02-05 07:19:44 +0000

    add read data from localstorage and reset button func

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
 // setup data structure
 
-const expenseTrackerData = {
+let expenseTrackerData = {
     income: [],
     expenses: []
 };
+
+// load data from local storage if it exists
+if (localStorage.getItem('expenseTrackerData')) {
+    expenseTrackerData = JSON.parse(localStorage.getItem('expenseTrackerData'));
+    console.log(expenseTrackerData);  // TODO: Remove in final version
+}
 
 // get data from the form on submit
 const form = document.querySelector('form');
@@ -13,7 +19,7 @@ form.addEventListener('submit', (e) => {
     const type = formData.get('type');
     const description = formData.get('description');
     const amount = formData.get('amount');
-    console.log(type, description, amount);  // check if the data is correct
+    console.log(type, description, amount);  // TODO: Remove in final version, check if the data is correct
     // separate data based on type and add it to the data structure
     if (type === 'income') {
         expenseTrackerData.income.push({ description, amount });
@@ -22,5 +28,11 @@ form.addEventListener('submit', (e) => {
     }
     // save data to local storage
     localStorage.setItem('expenseTrackerData', JSON.stringify(expenseTrackerData));
+    form.reset();
+});
+
+// Reset the form
+const resetBtn = document.querySelector('#resetBtn');
+resetBtn.addEventListener('click', () => {
     form.reset();
 });
